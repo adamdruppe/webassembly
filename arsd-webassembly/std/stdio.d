51e72787051b2fb006137bec6e6131fffbd12ae1
Hipreme <msnmancini@hotmail.com>
2023-01-06 13:05:26 -0300

    Requested changed

--- arsd-webassembly/std/stdio.d
+++ arsd-webassembly/std/stdio.d
@@ -4,6 +4,14 @@ import arsd.webassembly;
 
 void writeln(T...)(T t) {
 	eval(q{
-		console.log.apply(null, arguments);
+		var str = "";
+		for(var i = 0; i < arguments.length; i++)
+			str += arguments[i];
+
+		str += "\n";
+
+		var txt = document.createTextNode(str);
+		var fd = document.getElementById("stdout");
+		fd.appendChild(txt);
 	}, t);
 }
